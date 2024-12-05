d65e3ee2ab65b8bfdfbe5b6bf2f3a8177c756db7
Cruz Alfaro <cruzelcapo_@hotmail.com>
2024-12-05 20:18:04 -0300

    Prueba de Commit

--- Ejercicio Biotex/Form1.Designer.cs
+++ Ejercicio Biotex/Form1.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            helpProvider1 = new HelpProvider();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Location = new Point(50, 33);
+            button1.Name = "button1";
+            button1.Size = new Size(169, 63);
+            button1.TabIndex = 0;
+            button1.Text = "Presionar";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private HelpProvider helpProvider1;
     }
 }
--- Ejercicio Biotex/Form1.cs
+++ Ejercicio Biotex/Form1.cs
@@ -6,5 +6,10 @@ namespace Ejercicio_Biotex
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
